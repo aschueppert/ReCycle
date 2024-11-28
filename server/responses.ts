d1ff5242215da022369ce4b01f8c1522103b114a
Amelia Scheppert <ameliascheppert@amelias-air.lan>
2024-11-27 18:00:55 -0600

    redeploy

--- server/responses.ts
+++ server/responses.ts
@@ -4,7 +4,7 @@ import { PostAuthorNotMatchError, PostDoc } from "./concepts/posting";
 import { Router } from "./framework/router";
 
 /**
- * This class does useful conversions for the frontend.
+ * This class does   useful conversions for the frontend.
  * For example, it converts a {@link PostDoc} into a more readable format for the frontend.
  */
 export default class Responses {
