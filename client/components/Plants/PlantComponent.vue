f76d1a449eb03a74b6e40b05c4836cd91a0a6e37
Amelia Scheppert <ameliascheppert@dhcp-10-29-213-83.dyn.MIT.EDU>
2024-12-01 22:01:31 -0500

    Add plant component

---
+++ client/components/Plants/PlantComponent.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { fetchy } from "@/utils/fetchy";
+import { storeToRefs } from "pinia";
+import { onBeforeMount, ref } from "vue";
+
+// Accessing the isLoggedIn state from the store
+const { isLoggedIn } = storeToRefs(useUserStore());
+
+const cosmetics_ids: Record<string, string> = { "507f191e810c19729de860ea": "fas fa-seedling" };
+// Reactive states
+const loaded = ref(false);
+const seeds = ref();
+const cosmetics = ref();
+// Fetch the scores data
+async function getSeeds() {
+  try {
+    const seedResults = await fetchy(`/api/seeds`, "GET", {});
+    seeds.value = seedResults; // Assign the response to scores
+  } catch (_) {
+    seeds.value = 0; // Handle errors by resetting scores
+  }
+}
+
+async function getCosmetics() {
+  try {
+    const cosmeticsResults = await fetchy(`/api/cosmetics`, "GET", {});
+    cosmetics.value = cosmeticsResults; // Assign the response to scores
+  } catch (_) {
+    cosmetics.value = 0; // Handle errors by resetting scores
+  }
+}
+// Trigger an action to buy a plant
+async function buyPlant(item: string) {
+  try {
+    await fetchy(`/api/purchase`, "POST", { body: { item } });
+    await getSeeds(); // Refresh the scores after buying a plant
+    await getCosmetics();
+  } catch (_) {
+    // Error handling
+  }
+}
+async function classify() {
+  try {
+    await fetchy(`/api/classify`, "POST", {});
+    await getSeeds(); // Refresh the scores after classifying
+    await getCosmetics();
+  } catch (_) {
+    // Error handling
+  }
+}
+
+// Fetch scores when the component mounts
+onBeforeMount(async () => {
+  await getSeeds();
+  await getCosmetics();
+  loaded.value = true;
+});
+</script>
+
+<template>
+  <head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+  </head>
+  <section v-if="loaded">
+    <p>{{ seeds.value }}</p>
+    <article class="plants" v-for="item in cosmetics.items" :key="item">
+      <i :class="cosmetics_ids[item]"></i>
+    </article>
+    <p>2 seeds</p>
+    <i class="fas fa-seedling"></i>
+    <button @click="buyPlant('flower')">grow</button>
+    <button @click="classify">getSeeds</button>
+  </section>
+</template>
+
+<style scoped>
+.page {
+  align-items: center;
+  margin-top: 5em;
+}
+
+.plants {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap; /* Ensures items can wrap to the next line if necessary */
+  gap: 1em; /* Space between the plant items */
+  justify-content: center; /* Centers the items horizontally */
+}
+
+i {
+  font-size: 3em;
+  color: green;
+}
+
+button {
+  margin-top: 1em;
+}
+</style>
