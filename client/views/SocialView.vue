a96f88fa8693acce8d17cf5f9fc4ed72ec82c8b5
Richard Lin <earlight@Richards-MacBook-Pro.local>
2024-12-05 23:29:48 -0500

    final style for social view

--- client/views/SocialView.vue
+++ client/views/SocialView.vue
@@ -16,8 +16,8 @@ const switchView = (view: "leaderboard" | "activityList") => {
   <div class="toggle-view-container">
     <!-- Instagram-Style Toggle Buttons -->
     <div class="toggle-buttons">
-      <button :class="{ active: selectedView === 'leaderboard' }" @click="switchView('leaderboard')">Leaderboard</button>
-      <button :class="{ active: selectedView === 'activityList' }" @click="switchView('activityList')">Activity List</button>
+      <button :class="['button', { active: selectedView === 'leaderboard' }]" @click="switchView('leaderboard')">Leaderboard</button>
+      <button :class="['button', { active: selectedView === 'activityList' }]" @click="switchView('activityList')">Activity List</button>
     </div>
 
     <!-- Conditional Rendering of Components -->
@@ -41,16 +41,20 @@ const switchView = (view: "leaderboard" | "activityList") => {
   background: #f0f0f0;
   border-radius: 30px;
   padding: 0.2rem;
-  margin-bottom: 1rem;
   width: fit-content;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  gap: 0.5em;
+}
+
+.button {
+  margin-bottom: 0;
 }
 
 .toggle-buttons button {
   flex: 1;
   border: none;
   background: transparent;
-  padding: 0.5rem 1.5rem;
+  padding: 10px 20px;
   font-size: 1rem;
   font-weight: 600;
   color: #666;
@@ -59,14 +63,19 @@ const switchView = (view: "leaderboard" | "activityList") => {
   transition: all 0.3s ease;
 }
 
+.toggle-buttons button.hover {
+  background-color: #056635;
+}
+
 .toggle-buttons button.active {
-  background: linear-gradient(45deg, #55c58a, #44b076, #3caf72);
+  background-color: #044120;
   color: white;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
 }
 
 .toggle-buttons button:not(.active):hover {
-  color: #000;
+  transform: scale(1.1);
+  color: #044120;
 }
 
 .view-content {
